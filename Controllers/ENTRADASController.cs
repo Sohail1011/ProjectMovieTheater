ec28b1513f4843b0e466b912260c8ead04be0c7c
Sohail1011 <aratavalga@gmail.com>
2022-08-24 19:13:25 -0500

    project movie

---
+++ Controllers/ENTRADASController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// importar al modelo/s
+using ProyectoCine.Models;
+
+namespace ProyectoCine.Controllers
+{
+    public class ENTRADASController : Controller
+    {
+        static List<ENTRADAS> Lista = new List<ENTRADAS>();
+
+        List<string> Horarios = new List<string>()
+        { "16:00","18:00", "20:00","22:00"};
+
+        // GET: ENTRADASController
+        public ActionResult IndexEntradas() // Listado
+        {
+            // enviar los datos del modelo a la vista
+            return View(Lista);
+        }
+
+        // GET: ENTRADASController/Details/5
+        public ActionResult DetailsEntradas(int id) // Consulta de 1
+        {
+            return View();
+        }
+
+        // GET: ENTRADASController/Create
+        public ActionResult CreateEntradas() // Grabar
+        {
+            // creamos una nueva instancia del modelo ENTRADAS
+            ENTRADAS obj = new ENTRADAS();
+            obj.FechaEntrada = DateTime.Today;
+            obj.Cantidad = 1;
+
+            // enviamos el nuevo modelo a la vista
+            return View(obj);
+        }
+
+        // POST: ENTRADASController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateEntradas(ENTRADAS xEnt) // Grabar
+        {
+            // agregar la variable del objeto ENTRADAS a la lista
+            Lista.Add(xEnt);
+
+            // enviando un mensaje a la vista
+            // creando la variable
+            ViewBag.MENSAJE = "Nueva Entrada grabada correctamente";
+
+            return View(xEnt);
+        }
+
+        // GET: ENTRADASController/Edit/5
+        public ActionResult EditEntradas(int id)
+        {
+            return View();
+        }
+
+        // POST: ENTRADASController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditEntradas(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ENTRADASController/Delete/5
+        public ActionResult DeleteEntradas(int id)
+        {
+            return View();
+        }
+
+        // POST: ENTRADASController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteEntradas(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/ENTRADAS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoCine.Models
+{
+    public class ENTRADAS
+    {
+        // PROPIEDADES:
+        // digitar "prop" y presionar 2
+        // veces la tecla TAB
+        public int NroEntrada { get; set; }
+        public DateTime FechaEntrada { get; set; }
+        public string NombrePelicula { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Importe {
+            get {
+                return Precio * Cantidad;
+            }
+        }
+    }
+}
